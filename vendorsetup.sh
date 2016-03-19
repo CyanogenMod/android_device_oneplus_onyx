03aa2b045a30addcb1549b1b45a8b0b24516463f
SarthakNarang <sarthaknarang109@gmail.com>
2016-03-19 05:45:51 +0000

    AOSP bringup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo cm_onyx-user
-add_lunch_combo cm_onyx-userdebug
-add_lunch_combo cm_onyx-eng
+add_lunch_combo aosp_onyx-user
+add_lunch_combo aosp_onyx-userdebug
+add_lunch_combo aosp_onyx-eng
